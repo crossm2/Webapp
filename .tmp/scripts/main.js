5d41da3fc33f95db7cc9e23e238ac1ec1b8e8639
Marlie <crossm2@seattleu.edu>
2016-04-02 14:23:09 -0700

    added css html and javascript

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,4 +1,13 @@
 'use strict';
 
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$(document).ready(function () {
+    console.log("ready!");
+
+    $('li > a').click(function () {
+        $('li').removeClass();
+        $(this).parent().addClass('active');
+    });
+});
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+$('li > a').click(function() {
+    $('li').removeClass();
+    $(this).parent().addClass('active');
+});
+
+});
